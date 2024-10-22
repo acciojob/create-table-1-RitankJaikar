d6650dc45886b747bd62fa751ba13052985c6fe8
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-22 17:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-let i=1;
+
 function insert_Row() {
 	//Write your code here
 	let sampleTable= document.querySelector("#sampleTable");
 	let tr = document.createElement("tr");
 	
 	let td1 = document.createElement("td");
-	td1.innerText = `New Cell1${i}`;
-	i++;
+	td1.innerText = "New Cell1";
 	let td2 = document.createElement("td");
-	td2.innerText = `New Cell1${i}`;
+	td2.innerText = "New Cell2";
 
 	tr.append(td1);
 	tr.append(td2);
 	sampleTable.prepend(tr);
-
-	i++;
 }
